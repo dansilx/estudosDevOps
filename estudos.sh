9e233c4221e432bd1589e5ee2cecb7b36f77cb58
dansilx <danilosilveira@msn.com>
2025-01-04 16:07:26 -0300

    envinado via wsl

---
+++ estudos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ condicional ]; then
+    # Comandos a serem executados se a condicao testada for verdadeira
+elif [ outra condicional ]; then
+    # Comandos a serem executados se a primeira condicao testada for falsa e a segunda 
